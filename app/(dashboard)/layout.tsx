b8d884082a765e8b304666fd339b7a5f806e9cf0
Paulo Cesar Fordelone <pc@fordelone.com.br>
2023-10-14 14:15:26 -0300

    - initial layout and config updates

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,10 +1,32 @@
 import { Header } from '@/components/header'
+import prisma from '@/lib/prisma'
+import { auth } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
 
-export default function DashboardLayout({
+export default async function DashboardLayout({
   children,
+  params,
 }: {
   children: React.ReactNode
+  params: { musicianId: string }
 }) {
+  const { userId } = auth()
+
+  if (!userId) {
+    redirect('/sign-in')
+  }
+
+  const musician = await prisma.musician.findFirst({
+    where: {
+      id: params.musicianId,
+      user_id: userId,
+    },
+  })
+
+  if (!musician) {
+    redirect('/')
+  }
+
   return (
     <div className="h-screen w-full flex-col items-center justify-center">
       <Header />
